81fb41685f0f260b15f063d8daacba24ae9ff6ff
Arthurpanck <Arthurpanck@users.noreply.github.com>
2026-06-26 10:24:27 +0000

    Deploy QB demo [skip ci] 76caa12c82b45eec670ab393c460a05624fb7bdd

---
+++ app/dist/styles.cd4cbc9f3941e73d.js
@@ -0,0 +1,6 @@
+/*
+ * This file is subject to the terms and conditions defined in
+ * file 'LICENSE.txt', which is part of this source code package.
+ */
+
+"use strict";(self.rspackChunk=self.rspackChunk||[]).push([[14],{24207:function(){}},function(s){s(s.s=24207)}]);
